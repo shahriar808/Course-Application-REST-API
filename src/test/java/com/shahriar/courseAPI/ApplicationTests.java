8bb1d04b29bec384db5d41a0f9f139ee92fb8434
shahriar808 <shasan5525@gmail.com>
2024-07-08 23:43:06 +0600

    initial commit

---
+++ src/test/java/com/shahriar/courseAPI/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahriar.courseAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
